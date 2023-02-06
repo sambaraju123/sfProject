37359398efb8889edf932dce35d4f5c0ebbed253
sambaraju123 <ksamba2022@gmail.com>
2023-02-06 10:56:18 +0530

    commit

---
+++ force-app/main/default/lwc/lwcpro/lwcpro.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class Demo2 extends LightningElement {
+    call(event){
+        alert("my first coding journey");
+    }
+    caller(event){
+        alert("my first coding journey");
+    }
+}
